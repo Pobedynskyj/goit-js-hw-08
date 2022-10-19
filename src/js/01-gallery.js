05a3a59ed6a7f7bdf7755475dd0d6234996118bf
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-10-19 15:36:56 +0300

    add 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const ulEl = document.querySelector(`.gallery`);
+let allPictures = "";
+galleryItems.forEach((object) => {
+  allPictures += `<a class="gallery__item" href="${object.original}">
+  <img class="gallery__image" src="${object.preview}" alt="${object.description}" />
+</a>`;
+});
+
+ulEl.innerHTML = allPictures;
+
+ulEl.addEventListener(`click`, onClick);
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
+const lightBox = new SimpleLightbox(`.gallery a`, {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
 
 console.log(galleryItems);
