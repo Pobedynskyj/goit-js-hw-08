668c29e079222c9304764014a83a6de07cb87605
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-10-20 17:35:48 +0300

    add 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframeEl = document.querySelector(`#vimeo-player`);
+const player = new Player(iframeEl);
+
+player.on("timeupdate", throttle(getCurrentTime, 1000));
+
+function getCurrentTime({ seconds }) {
+  localStorage.setItem("currentSecond", seconds);
+}
+
+if (localStorage.getItem("currentSecond")) {
+  const currentTime = localStorage.getItem("currentSecond");
+  player.setCurrentTime(currentTime);
+}
