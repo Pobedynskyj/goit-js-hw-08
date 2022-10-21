89d3692972d7f3df27136354fcd0c8ec25847454
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-10-21 13:33:41 +0300

    add 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,10 +7,10 @@ const player = new Player(iframeEl);
 player.on("timeupdate", throttle(getCurrentTime, 1000));
 
 function getCurrentTime({ seconds }) {
-  localStorage.setItem("currentSecond", seconds);
+  localStorage.setItem("videoplayer-current-time", seconds);
 }
 
-if (localStorage.getItem("currentSecond")) {
-  const currentTime = localStorage.getItem("currentSecond");
+if (localStorage.getItem("videoplayer-current-time")) {
+  const currentTime = localStorage.getItem("videoplayer-current-time");
   player.setCurrentTime(currentTime);
 }
